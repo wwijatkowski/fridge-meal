f6c0b28ce6735ea30e80331bcefb289261af79dd
WIKTOR WIJATKOWSKI <victor.wijatkowski@gmail.com>
2021-09-09 18:09:40 +0200

    Menu added

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import json
 
 
 def main():
-    # choice_to_eat = input('what you want to eat? ')
-    choice_to_eat = 'scrambled eggs'
     # Load actual ingredients file list and meals.
+
     data_ingredients = json.load(open('ingredients.json'))
     data_meals = json.load(open('meals.json'))
+    dict_of_meals_with_no = dict(enumerate(list(data_meals.keys()), 1))
     dict_of_ingredients_needed = dict()
+
+    # Menu presenting
+    for no, meals in dict_of_meals_with_no.items():
+        print(f'{no} -> {meals}')
+    no_of_chosen_meal = input('What you want to eat? \n')
+
+    choice_to_eat = dict_of_meals_with_no[int(no_of_chosen_meal)]
 
     # Checking if we have enough quantity of each ingredient, if not create list of missing ingredients with quantity
     dict_ingredients_of_chosen_meal = data_meals.get(choice_to_eat)
@@ -25,6 +32,7 @@ def main():
     else:
         for ingredient, count in dict_ingredients_of_chosen_meal.items():
             data_ingredients[ingredient] -= count
+        print(f'Your meal "{choice_to_eat}" is ready. Bon appetit!')
 
     # Saving new quantity of ingredients into file json.
     data_ingredients_file = open('ingredients.json', "w")
